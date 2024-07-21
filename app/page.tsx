a9c4127cfbd7466533e440ec419b64db0f2b97f0
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-07-21 00:59:42 -0300

    changed some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Banner from "@/components/banner.client";
+import Link from "next/link";
 
 export default function Home() {
+  const coffeeStoreId = "meme-coffee";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Banner />
+    <main className="mb-56">
+      <div className="mx-auto mt-10 max-w-6xl px-4">
+        <Banner />
+        <Link href={`/store/${coffeeStoreId}`}>Meme coffee</Link>
+      </div>
     </main>
   );
 }
